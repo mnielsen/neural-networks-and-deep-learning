1decd10659a7bc919d71616f0368853db37fe93e
Michael Nielsen <mn@michaelnielsen.org>
2012-11-07 15:17:25 -0500

    Finished changing from list-based to numpy array-based data representation.  Doesn't much matter for this code, but in general it's better to use the vectorized approach.

--- perceptron_learning.py
+++ perceptron_learning.py
@@ -6,46 +6,41 @@ Demonstrates how a perceptron can learn the NAND gate, using the
 perceptron learning algorithm."""
 
 #### Libraries
-# Standard library
-import random
-
-# Third-part libraries
+# Third-party library
 import numpy
 
 class Perceptron():
     """ A Perceptron instance can take a function and attempt to
-    ``learn`` a bias and set of weights that compute that function, using
-    the perceptron learning algorithm. """
+    ``learn`` a bias and set of weights that compute that function,
+    using the perceptron learning algorithm."""
 
     def __init__(self, num_inputs=2):
-        """ Initialize the perceptron with the bias and all weights set
-        to 0.0. ``num_inputs`` is the number of input bits to the
+        """ Initialize the perceptron with the bias and all weights
+        set to 0.0. ``num_inputs`` is the number of input bits to the
         perceptron."""
         self.num_inputs = num_inputs
         self.bias = 0.0
-        self.weights = numpy.zeros((num_inputs,1))
+        self.weights = numpy.zeros(num_inputs)
         # self.inputs is a convenience attribute.  It's a list containing
         # all possible binary inputs to the perceptron.  E.g., for three
-        # inputs it is: [[0, 0, 0], [0, 0, 1], ...]
-        self.inputs = [[int(y)
-                        for y in bin(x).lstrip("0b").zfill(num_inputs)]
+        # inputs it is: [numpy.array([0, 0, 0]), numpy.array([0, 0, 1]), ...]
+        self.inputs = [numpy.array([int(y)
+                        for y in bin(x).lstrip("0b").zfill(num_inputs)])
                        for x in xrange(2**num_inputs)]      
 
     def output(self, x):
         """ Return the output (0 or 1) from the perceptron, with input
         ``x``."""
-        return 1 if sum(wt*y for (wt, y) in zip(self.weights, x)) + \
-               self.bias > 0 else 0
+        return 1 if numpy.inner(self.weights, x)+self.bias > 0 else 0
 
     def learn(self, f, eta=0.1):
         """ Find a bias and a set of weights for a perceptron that
         computes the function ``f``. ``eta`` is the learning rate, and
-        should be a small positive number.  Does not terminate when the
-        function cannot be computed using a perceptron."""        
+        should be a small positive number.  Does not terminate when
+        the function cannot be computed using a perceptron."""        
         # initialize the bias and weights with random values
-        self.bias = random.gauss(0.0, 1.0)
-        self.weights = [random.gauss(0.0, 1.0)
-                        for x in xrange(self.num_inputs)]
+        self.bias = numpy.random.normal()
+        self.weights = numpy.random.randn(self.num_inputs)
         number_of_errors = -1
         while number_of_errors != 0:         
             number_of_errors = 0
@@ -58,8 +53,7 @@ class Perceptron():
                 if error:
                     number_of_errors += 1
                     self.bias = self.bias+eta*error
-                    self.weights = [self.weights[j]+eta*error*x[j]
-                                    for j in xrange(self.num_inputs)]                 
+                    self.weights = self.weights+eta*error*x
             print "Number of errors:", number_of_errors, "\n"
 
 def f(x): 
