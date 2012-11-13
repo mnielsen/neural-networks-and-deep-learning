8281a482ff10c6149df0d902d8a28ca1e582541f
Michael Nielsen <mn@michaelnielsen.org>
2012-11-13 08:35:59 -0500

    Spacing

---
+++ sigmoid.py
@@ -0,0 +1,24 @@
+"""
+sigmoid
+~~~~~~~
+
+Plots a graph of the sigmoid function."""
+
+import numpy
+import matplotlib.pyplot as plt
+
+z = numpy.arange(-5, 5, .1)
+sigma_fn = numpy.vectorize(lambda z: 1/(1+numpy.exp(-z)))
+sigma = sigma_fn(z)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(z, sigma)
+ax.set_ylim([0,1])
+ax.set_xlim([-5,5])
+ax.grid(False)
+ax.set_xlabel('z')
+ax.set_ylabel('sigma(z)')
+ax.set_title('Sigma function')
+
+plt.show()
