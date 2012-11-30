f8e1ae6cc01d1828da4bb5f9ac30b14970424088
Michael Nielsen <mn@michaelnielsen.org>
2012-11-30 08:58:25 -0500

    Fixing signs

--- bad_online_learning.py
+++ bad_online_learning.py
@@ -12,7 +12,7 @@ import numpy
 X = numpy.arange(-1, 1, 0.05)
 Y = numpy.arange(-1, 1, 0.05)
 X, Y = numpy.meshgrid(X, Y)
-Z = -X**2 - Y**2 + numpy.exp(-4*X**2+0.3*Y+X)
+Z = X**2 + Y**2 - numpy.exp(-4*X**2+0.3*Y+X)
 
 plt.figure()
 CS = plt.contour(X, Y, Z)
