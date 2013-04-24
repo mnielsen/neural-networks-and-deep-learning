396295504e144a72b57029853fc7da64ca75cb5a
Michael Nielsen <mn@michaelnielsen.org>
2013-04-24 13:34:50 -0400

    Better images for MNIST autoencoder

--- code/mnist_autoencoder.py
+++ code/mnist_autoencoder.py
@@ -15,23 +15,19 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Constants
-HIDDEN_UNITS=100 # size of the hidden layer in the autoencoder
-
-def main():
+def mnist_autoencoder(hidden_units):
     # Do the training
     training_data, test_inputs, actual_test_results = \
         mnist_loader.load_data_nn()
     autoencoder_training_data = [(x, x) for x, _ in training_data]
-    net = Network([784, HIDDEN_UNITS, 784])
+    net = Network([784, hidden_units, 784])
     net.SGD(autoencoder_training_data, 3, 10, 0.01, 0.05)
-    # Plot the first ten outputs
+    # Plot the first ten test outputs
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    images_in = [autoencoder_training_data[j][0].reshape(-1, 28)
-                 for j in range(10)]
-    images_out = [net.feedforward(autoencoder_training_data[j][0]).\
-                      reshape(-1, 28) for j in range(10)]
+    images_in = [test_inputs[j].reshape(-1, 28) for j in range(10)]
+    images_out = [net.feedforward(test_inputs[j]).reshape(-1, 28) 
+                  for j in range(10)]
     image_in = np.concatenate(images_in, axis=1)
     image_out = np.concatenate(images_out, axis=1)
     image = np.concatenate([image_in, image_out])
@@ -39,6 +35,3 @@ def main():
     plt.xticks(np.array([]))
     plt.yticks(np.array([]))
     plt.show()
-
-if __name__ == "__main__":
-    main()
