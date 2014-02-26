b56c749195ecc519559252d973c4be1d0369062b
Michael Nielsen <mn@michaelnielsen.org>
2014-02-26 13:47:42 -0500

    Fixed typo

--- code/network_basic.py
+++ code/network_basic.py
@@ -67,7 +67,7 @@ class Network():
                 print "Epoch %s complete" % j
 
     def update_mini_batch(self, mini_batch, eta):
-        """Update the network's weights and biases by applying a
+        """Update the network's weights and biases by applying
         gradient descent using backpropagation to a single mini batch.
         The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
         is the learning rate."""
