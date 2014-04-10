e4384ccbf88d88d15e7b5b9e86282bf7c0743834
Michael Nielsen <mn@michaelnielsen.org>
2014-04-10 15:26:22 -0400

    Tweaking of the overfitting

--- fig/overfitting.py
+++ fig/overfitting.py
@@ -29,13 +29,11 @@ evaluation_cost, evaluation_accuracy, training_cost, training_accuracy = \
             monitor_training_accuracy=True)
 
 # Do the plots
-epochs = np.arange(40, 150, 1)
 
 #Plot the training cost data
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(epochs, training_cost[40:150])
-ax.set_ylim([0, 100])
+ax.plot(np.arange(40, 150, 1), training_cost[40:150])
 ax.set_xlim([40, 150])
 ax.grid(True)
 ax.set_xlabel('Epoch')
@@ -45,7 +43,8 @@ plt.show()
 # Plot the test accuracy data
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(epochs, [accuracy/100.0 for accuracy in evaluation_accuracy[40:150]])
+ax.plot(np.arange(40, 150, 1), 
+        [accuracy/100.0 for accuracy in evaluation_accuracy[40:150]])
 ax.set_xlim([40, 150])
 ax.grid(True)
 ax.set_xlabel('Epoch')
@@ -55,19 +54,19 @@ plt.show()
 #Plot the test cost data
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(epochs, evaluation_cost[40:150])
+ax.plot(np.arange(40, 150, 1), evaluation_cost[40:150])
 ax.set_xlim([40, 150])
 ax.grid(True)
 ax.set_xlabel('Epoch')
-ax.set_title('Cost on the training data')
+ax.set_title('Cost on the test data')
 plt.show()
 
 # Plot the training accuracy data
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(epochs, [accuracy/100.0 for accuracy in training_accuracy[40:150]])
-ax.set_xlim([40, 150])
+ax.plot(np.arange(0, 150, 1), [accuracy/100.0 for accuracy in training_accuracy[40:150]])
+ax.set_xlim([0, 150])
 ax.grid(True)
 ax.set_xlabel('Epoch')
-ax.set_title('Accuracy (%) on the test data')
+ax.set_title('Accuracy (%) on the training data')
 plt.show()
