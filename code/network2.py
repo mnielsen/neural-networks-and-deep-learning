04d441d8c0d960fb9f89f487d3652822129bb235
Michael Nielsen <mn@michaelnielsen.org>
2014-04-10 15:00:19 -0400

    Improved monitoring ability in network2, and did more comprehensive graphs in overfitting

--- code/network2.py
+++ code/network2.py
@@ -78,23 +78,30 @@ class Network():
             a = sigmoid_vec(np.dot(w, a)+b)
         return a
 
-    def SGD(self, training_data, epochs, mini_batch_size, eta,
-            lmbda = 0.0, print_cost=False, data=None):
+    def SGD(self, training_data, epochs, mini_batch_size, eta, lmbda = 0.0, 
+            evaluation_data=None, monitor_evaluation_cost=False,
+            monitor_evaluation_accuracy=False,
+            monitor_training_cost=False, monitor_training_accuracy=False):
         """Train the neural network using mini-batch stochastic gradient
         descent.  The ``training_data`` is a list of tuples ``(x, y)``
         representing the training inputs and the desired outputs.  The
         other non-optional parameters are self-explanatory, as is the
-        regularization parameter ``lmbda``.  If ``print_cost`` is true
-        then the cost is printed after each epoch.  If ``data`` is
-        provided then the network will be evaluated against that data
-        after each epoch, and partial progress printed out.  This is
-        useful for tracking progress, but slows things down
-        substantially.  Note that ``data`` will usually be either the
-        validation or test data.
+        regularization parameter ``lmbda``.  The method also accepts
+        ``evaluation_data``, usually either the validation or test
+        data.  We can monitor the cost and accuracy on either the
+        evaluation data or the training data, by setting the
+        appropriate flags.  The method returns a tuple containing four
+        lists: the costs on the evaluation data, the accuracies on the
+        evaluation data, the costs on the training data, and the
+        accuracies on the training data.  All values are evaluated at
+        the end of each training epoch.  Note that the lists are empty
+        if the corresponding flag is not set.
 
         """
-        if data: n_data = len(data)
+        if evaluation_data: n_data = len(evaluation_data)
         n = len(training_data)
+        evaluation_cost, evaluation_accuracy = [], []
+        training_cost, training_accuracy = [], []
         for j in xrange(epochs):
             random.shuffle(training_data)
             mini_batches = [
@@ -102,13 +109,28 @@ class Network():
                 for k in xrange(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta, lmbda)
-            if print_cost:
-                print "Epoch {} cost on training data: {}".format(
-                    j, self.total_cost(training_data, lmbda))
-            if data:
-                print "Epoch {} accuracy on supplied data: {} / {}".format(
-                    j, self.evaluate(data), n_data)
             print "Epoch %s complete" % j
+            if monitor_training_cost:
+                cost = self.total_cost(training_data, lmbda)
+                training_cost.append(cost)
+                print "Cost on training data: {}".format(cost)
+            if monitor_training_accuracy:
+                accuracy = self.accuracy(training_data, convert=True)
+                training_accuracy.append(accuracy)
+                print "Accuracy on training data: {} / {}".format(
+                    accuracy, n)
+            if monitor_evaluation_cost:
+                cost = self.total_cost(evaluation_data, lmbda, convert=True)
+                evaluation_cost.append(cost)
+                print "Cost on evaluation data: {}".format(cost)
+            if monitor_evaluation_accuracy:
+                accuracy = self.accuracy(evaluation_data)
+                evaluation_accuracy.append(accuracy)
+                print "Accuracy on evaluation data: {} / {}".format(
+                    self.accuracy(evaluation_data), n_data)
+            print
+        return evaluation_cost, evaluation_accuracy, \
+            training_cost, training_accuracy
 
     def update_mini_batch(self, mini_batch, eta, lmbda):
         """Update the network's weights and biases by applying gradient
@@ -160,24 +182,53 @@ class Network():
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
         return (nabla_b, nabla_w)
 
-    def evaluate(self, test_data):
-        """Return the number of test inputs for which the neural
-        network outputs the correct result. Note that the neural
-        network's output is assumed to be the index of whichever
-        neuron in the final layer has the highest activation."""
-        test_results = [(np.argmax(self.feedforward(x)), y) 
-                        for (x, y) in test_data]
-        return sum(int(x == y) for (x, y) in test_results)
+    def accuracy(self, data, convert=False):
+        """Return the number of inputs in ``data`` for which the neural
+        network outputs the correct result. The neural network's
+        output is assumed to be the index of whichever neuron in the
+        final layer has the highest activation.  The flag ``convert``
+        should be set to False if the data set is validation or test
+        data (the usual case), and to True if the data set is the
+        training data. The need for this flag arises due to
+        differences in the way the results ``y`` are represented ---
+        it flags whether we need to convert between the different
+        representations.
 
-    def total_cost(self, training_data, lmbda):
-        c = 0
-        for x, y in training_data:
+        """
+        if convert:
+            results = [(np.argmax(self.feedforward(x)), np.argmax(y)) 
+                       for (x, y) in data]
+        else:
+            results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in data]
+        return sum(int(x == y) for (x, y) in results)
+
+    def total_cost(self, data, lmbda, convert=False):
+        """Return the total cost for the data set ``data``.  The flag
+        ``convert`` should be set to False if the data set is the
+        training data (the usual case), and to True if the data set is
+        the validation or test data.  See comments on the similar (but
+        reversed) convention for the ``accuracy`` method, above.
+
+        """
+        cost = 0
+        for x, y in data:
             a = self.feedforward(x)
-            c += self.cost.fn(a, y)
-        c += 0.5*sum(np.linalg.norm(w)**2 for w in self.weights)
-        return c
+            if convert: y = vectorized_result(y)
+            cost += self.cost.fn(a, y)
+        cost += 0.5*lmbda*sum(np.linalg.norm(w)**2 for w in self.weights)
+        return cost
         
 #### Miscellaneous functions
+def vectorized_result(j):
+    """Return a 10-dimensional unit vector with a 1.0 in the jth
+    position and zeroes elsewhere.  This is used to convert a digit
+    (0...9) into a corresponding desired output from the neural
+    network."""
+    e = np.zeros((10, 1))
+    e[j] = 1.0
+    return e
+
 def sigmoid(z):
     """The sigmoid function."""
     return 1.0/(1.0+np.exp(-z))
--- fig/overfitting.py
+++ fig/overfitting.py
@@ -5,60 +5,68 @@ overfitting
 Plot graphs to illustrate the problem of overfitting.
 """
 
+# Standard library
+import imp
+
+# My library
+mnist_loader = imp.load_source('mnist_loader', '../code/mnist_loader.py')
+network2 = imp.load_source('network2', '../code/network2.py')
+
 # Third-party libraries
 import matplotlib.pyplot as plt
 import numpy as np
 
-epochs = np.arange(40, 100, 1)
 
-# The cost and accuracy data is generated using network2.py
-cost = [95.64940534, 92.77003957, 91.19426513, 90.05272327,
-        86.65961805, 83.72109886, 82.23502042, 79.32343131,
-        77.48936465, 76.45793827, 74.29915603, 70.78666687,
-        69.14013225, 67.60759576, 65.92640537, 64.37140944,
-        62.64708059, 60.8062498, 59.73079664, 58.39325199,
-        57.90915805, 55.93226466, 54.80526382, 53.84637318,
-        52.58869068, 51.52430656, 50.64235016, 49.68848763,
-        48.71468605, 47.8280628, 47.00485029, 46.28861009,
-        45.81707749, 44.70239253, 43.85854394, 43.67765765,
-        42.34793966, 41.7182567, 41.07244276, 40.71782626,
-        40.07246729, 39.17786677, 38.78476657, 38.04577764,
-        37.51645259, 36.97442833, 36.44307367, 35.91951676,
-        35.48611018, 34.96091884, 34.59400404, 33.96090448,
-        33.56307762, 33.1954933, 32.72731425, 32.22339415,
-        31.79197639, 31.72643205, 31.03274438, 30.71485425]
+# Generate the results
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost())
+evaluation_cost, evaluation_accuracy, training_cost, training_accuracy = \
+    net.SGD(training_data[:1000], 150, 10, 0.05,
+            evaluation_data=test_data, 
+            monitor_evaluation_cost=True, 
+            monitor_evaluation_accuracy=True, 
+            monitor_training_cost=True, 
+            monitor_training_accuracy=True)
 
-accuracy = [82.92, 82.78, 82.93, 82.94, 82.92,
-            82.98, 83.07, 83.05, 82.92, 83.02,
-            83.37, 83.24, 82.94, 83.11, 83.07,
-            83.34, 83.34, 83.27, 83.25, 83.31,
-            83.44, 83.33, 83.41, 83.32, 83.30,
-            83.30, 83.34, 83.55, 83.53, 83.51,
-            83.35, 83.37, 83.41, 83.54, 83.45,
-            83.50, 83.44, 83.50, 83.44, 83.48,
-            83.37, 83.49, 83.41, 83.47, 83.42,
-            83.46, 83.51, 83.54, 83.55, 83.38,
-            83.48, 83.49, 83.57, 83.56, 83.39,
-            83.32, 83.41, 83.54, 83.32, 83.41]
-            
+# Do the plots
+epochs = np.arange(40, 150, 1)
 
-#Plot the cost data
+#Plot the training cost data
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(epochs, cost)
+ax.plot(epochs, training_cost[40:150])
 ax.set_ylim([0, 100])
-ax.set_xlim([40, 100])
+ax.set_xlim([40, 150])
 ax.grid(True)
 ax.set_xlabel('Epoch')
 ax.set_title('Cost on the training data')
 plt.show()
 
-# Plot the accuracy data
+# Plot the test accuracy data
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(epochs, accuracy)
-ax.set_ylim([82, 84])
-ax.set_xlim([40, 100])
+ax.plot(epochs, [accuracy/100.0 for accuracy in evaluation_accuracy[40:150]])
+ax.set_xlim([40, 150])
+ax.grid(True)
+ax.set_xlabel('Epoch')
+ax.set_title('Accuracy (%) on the test data')
+plt.show()
+
+#Plot the test cost data
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(epochs, evaluation_cost[40:150])
+ax.set_xlim([40, 150])
+ax.grid(True)
+ax.set_xlabel('Epoch')
+ax.set_title('Cost on the training data')
+plt.show()
+
+# Plot the training accuracy data
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(epochs, [accuracy/100.0 for accuracy in training_accuracy[40:150]])
+ax.set_xlim([40, 150])
 ax.grid(True)
 ax.set_xlabel('Epoch')
 ax.set_title('Accuracy (%) on the test data')
