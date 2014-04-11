415eea2c625c8e8cd9f3f9d1d0fe79f73714e37c
Michael Nielsen <mn@michaelnielsen.org>
2014-04-11 12:24:28 -0400

    Improved overfitting results: more graphs, faster, easier to reproduce

--- fig/overfitting.py
+++ fig/overfitting.py
@@ -7,6 +7,7 @@ Plot graphs to illustrate the problem of overfitting.
 
 # Standard library
 import imp
+import json
 
 # My library
 mnist_loader = imp.load_source('mnist_loader', '../code/mnist_loader.py')
@@ -17,56 +18,80 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Generate the results
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost())
-evaluation_cost, evaluation_accuracy, training_cost, training_accuracy = \
-    net.SGD(training_data[:1000], 150, 10, 0.05,
-            evaluation_data=test_data, 
-            monitor_evaluation_cost=True, 
-            monitor_evaluation_accuracy=True, 
-            monitor_training_cost=True, 
-            monitor_training_accuracy=True)
+# Number of epochs to train for 
+NUM_EPOCHS = 100
 
-# Do the plots
+# Make results more easily reproducible
+import random
+random.seed(12345678)
 
-#Plot the training cost data
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(np.arange(40, 150, 1), training_cost[40:150])
-ax.set_xlim([40, 150])
-ax.grid(True)
-ax.set_xlabel('Epoch')
-ax.set_title('Cost on the training data')
-plt.show()
 
-# Plot the test accuracy data
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(np.arange(40, 150, 1), 
-        [accuracy/100.0 for accuracy in evaluation_accuracy[40:150]])
-ax.set_xlim([40, 150])
-ax.grid(True)
-ax.set_xlabel('Epoch')
-ax.set_title('Accuracy (%) on the test data')
-plt.show()
+def main():
+    run_network()
+    make_plots()
+                       
+def run_network():
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost())
+    test_cost, test_accuracy, training_cost, training_accuracy \
+        = net.SGD(training_data[:1000], NUM_EPOCHS, 10, 0.05,
+                  evaluation_data=test_data, 
+                  monitor_evaluation_cost=True, 
+                  monitor_evaluation_accuracy=True, 
+                  monitor_training_cost=True, 
+                  monitor_training_accuracy=True)
+    f = open("overfitting_results.json", "w")
+    json.dump([test_cost, test_accuracy, training_cost, training_accuracy], f)
+    f.close()
 
-#Plot the test cost data
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(np.arange(40, 150, 1), evaluation_cost[40:150])
-ax.set_xlim([40, 150])
-ax.grid(True)
-ax.set_xlabel('Epoch')
-ax.set_title('Cost on the test data')
-plt.show()
+def make_plots():
+    f = open("overfitting_results.json", "r")
+    test_cost, test_accuracy, training_cost, training_accuracy \
+        = json.load(f)
+    f.close()
+    plot_training_cost(training_cost)
+    plot_test_accuracy(test_accuracy)
+    plot_test_cost(test_cost)
+    plot_training_accuracy(training_accuracy)
 
-# Plot the training accuracy data
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(np.arange(0, 150, 1), [accuracy/100.0 for accuracy in training_accuracy[40:150]])
-ax.set_xlim([0, 150])
-ax.grid(True)
-ax.set_xlabel('Epoch')
-ax.set_title('Accuracy (%) on the training data')
-plt.show()
+def plot_training_cost(training_cost):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(40, NUM_EPOCHS, 1), training_cost[40:NUM_EPOCHS])
+    ax.set_xlim([40, NUM_EPOCHS])
+    ax.grid(True)
+    ax.set_xlabel('Epoch')
+    ax.set_title('Cost on the training data')
+    plt.show()
+
+def plot_test_accuracy(test_accuracy):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(40, NUM_EPOCHS, 1), 
+            [accuracy/100.0 for accuracy in evaluation_accuracy[40:NUM_EPOCHS]])
+    ax.set_xlim([40, NUM_EPOCHS])
+    ax.grid(True)
+    ax.set_xlabel('Epoch')
+    ax.set_title('Accuracy (%) on the test data')
+    plt.show()
+
+def plot_test_cost(test_cost):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(0, NUM_EPOCHS, 1), evaluation_cost[0:NUM_EPOCHS])
+    ax.set_xlim([0, NUM_EPOCHS])
+    ax.grid(True)
+    ax.set_xlabel('Epoch')
+    ax.set_title('Cost on the test data')
+    plt.show()
+
+def plot_training_accuracy(training_accuracy):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(0, NUM_EPOCHS, 1), 
+            [accuracy/10.0 for accuracy in training_accuracy])
+    ax.set_xlim([0, NUM_EPOCHS])
+    ax.grid(True)
+    ax.set_xlabel('Epoch')
+    ax.set_title('Accuracy (%) on the training data')
+    plt.show()
