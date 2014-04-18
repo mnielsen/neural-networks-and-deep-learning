3d74d841fa5d443571459142ac4c053d24943641
Michael Nielsen <mn@michaelnielsen.org>
2014-04-18 10:46:43 -0400

    Refactoring network2.py, and added new option for weight initialization

--- code/network2.py
+++ code/network2.py
@@ -4,7 +4,7 @@
 An improved version of network1, implementing the stochastic gradient
 descent learning algorithm for a feedforward neural network.
 Improvements include the addition of the cross-entropy cost function,
-regularization, and improved initialization of network weights.  Note
+regularization, and better initialization of network weights.  Note
 that I have focused on making the code simple, easily readable, and
 easily modifiable.  It is not optimized, and omits many desirable
 features.
@@ -23,26 +23,30 @@ import numpy as np
 
 class QuadraticCost:
 
-    def fn(self, a, y):
+    @staticmethod
+    def fn(a, y):
         """Return the cost associated with an output ``a`` and desired output
         ``y``.
         """
         return 0.5*np.linalg.norm(a-y)**2
 
-    def delta(self, z, a, y):
+    @staticmethod
+    def delta(cls, z, a, y):
         "Return the error delta from the output layer."
         return (a-y) * sigmoid_prime_vec(z)
 
 
 class CrossEntropyCost:
 
-    def fn(self, a, y):
+    @staticmethod
+    def fn(cls, a, y):
         """Return the cost associated with an output ``a`` and desired output
         ``y``.
         """
         return np.sum(-y*np.log(a)-(1-y)*np.log(1-a))
 
-    def delta(self, z, a, y):
+    @staticmethod
+    def delta(cls, z, a, y):
         """Return the error delta from the output layer.  Note that the
         variable z is not used in the function.  It is included in the
         function's parameters in order to make the parameters consistent
@@ -55,22 +59,57 @@ class CrossEntropyCost:
 class Network():
 
     def __init__(self, sizes, cost=CrossEntropyCost()):
-        """The list ``sizes`` contains the number of neurons in the
-        respective layers of the network.  For example, if the list
-        was [2, 3, 1] then it would be a three-layer network, with the
-        first layer containing 2 neurons, the second layer 3 neurons,
-        and the third layer 1 neuron.  The biases and weights for the
-        network are initialized randomly, using a Gaussian
-        distribution with mean 0, and variance 1.  Note that the first
-        layer is assumed to be an input layer, and by convention we
-        won't set any biases for those neurons, since biases are only
-        ever used in computing the outputs from later layers."""
+        """The list ``sizes`` contains the number of neurons in the respective
+        layers of the network.  For example, if the list was [2, 3, 1]
+        then it would be a three-layer network, with the first layer
+        containing 2 neurons, the second layer 3 neurons, and the
+        third layer 1 neuron.  The biases and weights for the network
+        are initialized randomly, using
+        ``self.default_weight_initializer`` (see docstring for that
+        method).
+
+        """
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-        self.weights = [np.random.randn(y, x) 
-                        for x, y in zip(sizes[:-1], sizes[1:])]
+        self.default_weight_initializer()
         self.cost=cost
+
+    def default_weight_initializer(self):
+        """Initialize the weights using a Gaussian distribution with mean 0
+        and standard deviation 1 divided by the square root of the
+        number of neurons input to the weight layer.  Initialize the
+        biases using a Gaussian distribution with mean 0 and standard
+        deviation 1.
+
+        Note that the first layer is assumed to be an input layer, and
+        by convention we won't set any biases for those neurons, since
+        biases are only ever used in computing the outputs from later
+        layers.
+
+        """
+        self.biases = [np.random.randn(y, 1) for y in self.sizes[1:]]
+        self.weights = [np.random.randn(y, x)/np.sqrt(x) 
+                        for x, y in zip(self.sizes[:-1], self.sizes[1:])]
+
+    def large_weight_initializer(self):
+        """Initialize the weights using a Gaussian distribution with mean 0
+        and standard deviation 1.  Initialize the biases using a
+        Gaussian distribution with mean 0 and standard deviation 1.
+
+        Note that the first layer is assumed to be an input layer, and
+        by convention we won't set any biases for those neurons, since
+        biases are only ever used in computing the outputs from later
+        layers.
+
+        This weight and bias initializer uses the same approach as in
+        Chapter 1, and is included for purposes of comparison.  It
+        will usually be better to use the default weight initializer
+        instead.
+
+        """
+        self.biases = [np.random.randn(y, 1) for y in self.sizes[1:]]
+        self.weights = [np.random.randn(y, x) 
+                        for x, y in zip(self.sizes[:-1], self.sizes[1:])]
 
     def feedforward(self, a):
         "Return the output of the network if ``a`` is input."
