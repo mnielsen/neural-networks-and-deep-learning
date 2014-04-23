5e94a07dd7f5631f901d7bd1561f552213ffc6f6
Michael Nielsen <mn@michaelnielsen.org>
2014-04-23 08:52:04 -0400

    Adding results for overfitting on full data set (much less is going on), plus a new graph

--- fig/overfitting.py
+++ fig/overfitting.py
@@ -81,11 +81,14 @@ def make_plots(filename, num_epochs,
     plot_test_cost(test_cost, num_epochs, test_cost_xmin)
     plot_training_accuracy(training_accuracy, num_epochs, 
                            training_accuracy_xmin, training_set_size)
+    plot_overlay(test_accuracy, training_accuracy, num_epochs,
+                 min(test_accuracy_xmin, training_accuracy_xmin),
+                 training_set_size)
 
 def plot_training_cost(training_cost, num_epochs, training_cost_xmin):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.plot(np.arange(training_cost_xmin, num_epochs, 1), 
+    ax.plot(np.arange(training_cost_xmin, num_epochs), 
             training_cost[training_cost_xmin:num_epochs],
             color='#2A6EA6')
     ax.set_xlim([training_cost_xmin, num_epochs])
@@ -97,7 +100,7 @@ def plot_training_cost(training_cost, num_epochs, training_cost_xmin):
 def plot_test_accuracy(test_accuracy, num_epochs, test_accuracy_xmin):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.plot(np.arange(test_accuracy_xmin, num_epochs, 1), 
+    ax.plot(np.arange(test_accuracy_xmin, num_epochs), 
             [accuracy/100.0 
              for accuracy in test_accuracy[test_accuracy_xmin:num_epochs]],
             color='#2A6EA6')
@@ -110,7 +113,7 @@ def plot_test_accuracy(test_accuracy, num_epochs, test_accuracy_xmin):
 def plot_test_cost(test_cost, num_epochs, test_cost_xmin):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.plot(np.arange(test_cost_xmin, num_epochs, 1), 
+    ax.plot(np.arange(test_cost_xmin, num_epochs), 
             test_cost[test_cost_xmin:num_epochs],
             color='#2A6EA6')
     ax.set_xlim([test_cost_xmin, num_epochs])
@@ -123,7 +126,7 @@ def plot_training_accuracy(training_accuracy, num_epochs,
                            training_accuracy_xmin, training_set_size):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.plot(np.arange(0, num_epochs, 1), 
+    ax.plot(np.arange(training_accuracy_xmin, num_epochs), 
             [accuracy*100.0/training_set_size 
              for accuracy in training_accuracy[training_accuracy_xmin:num_epochs]],
             color='#2A6EA6')
@@ -131,6 +134,26 @@ def plot_training_accuracy(training_accuracy, num_epochs,
     ax.grid(True)
     ax.set_xlabel('Epoch')
     ax.set_title('Accuracy (%) on the training data')
+    plt.show()
+
+def plot_overlay(test_accuracy, training_accuracy, num_epochs, xmin,
+                 training_set_size):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(xmin, num_epochs), 
+            [accuracy/100.0 for accuracy in test_accuracy], 
+            color='#2A6EA6',
+            label="Accuracy on the test data")
+    ax.plot(np.arange(xmin, num_epochs), 
+            [accuracy*100.0/training_set_size 
+             for accuracy in training_accuracy], 
+            color='#FFA933',
+            label="Accuracy on the training data")
+    ax.grid(True)
+    ax.set_xlim([xmin, num_epochs])
+    ax.set_xlabel('Epoch')
+    ax.set_ylim([90, 100])
+    plt.legend(loc="lower right")
     plt.show()
 
 if __name__ == "__main__":
