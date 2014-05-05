40c00750fb950d76da2cdf2f3a49ee26dc2e31a5
Michael Nielsen <mn@michaelnielsen.org>
2014-05-05 12:12:41 -0400

    Added save method and load function

--- code/network2.py
+++ code/network2.py
@@ -13,6 +13,7 @@ features.
 
 #### Libraries
 # Standard library
+import json
 import random
 
 # Third-party libraries
@@ -274,7 +275,32 @@ class Network():
             cost += self.cost.fn(a, y)
         cost += 0.5*lmbda*sum(np.linalg.norm(w)**2 for w in self.weights)
         return cost
-        
+
+    def save(self, filename):
+        """Save the neural network to the file ``filename``."""
+        data = {"sizes": self.sizes,
+                "weights": [w.tolist() for w in self.weights],
+                "biases": [b.tolist() for b in self.biases],
+                "cost": str(self.cost.__name__)}
+        f = open(filename, "w")
+        json.dump(data, f)
+        f.close()
+
+#### Loading a Network
+def load(filename):
+    """Load a neural network from the file ``filename``.  Returns an
+    instance of Network.
+
+    """
+    f = open(filename, "r")
+    data = json.load(f)
+    f.close()
+    cost = getattr(sys.modules[__name__], data["cost"])
+    net = Network(data["sizes"], cost=cost)
+    net.weights = [np.array(w) for w in data["weights"]]
+    net.biases = [np.array(b) for b in data["biases"]]
+    return net
+
 #### Miscellaneous functions
 def vectorized_result(j):
     """Return a 10-dimensional unit vector with a 1.0 in the j'th position
