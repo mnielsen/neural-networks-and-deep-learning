b99eb3497aa375cf2b7a3d6edb07e2b3efeda506
Michael Nielsen <mn@michaelnielsen.org>
2014-06-17 09:15:31 -0400

    Simple fix for Python 2.6

--- code/network.py
+++ code/network.py
@@ -61,7 +61,7 @@ class Network():
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
             if test_data:
-                print "Epoch {}: {} / {}".format(
+                print "Epoch {0}: {1} / {2}".format(
                     j, self.evaluate(test_data), n_test)
             else:
                 print "Epoch %s complete" % j
