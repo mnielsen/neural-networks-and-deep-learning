9bcaa8b8f4fa16975f0b692e16a11dc6d6c502cf
Michael Nielsen <mn@michaelnielsen.org>
2015-01-06 12:51:48 -0500

    Fixing the basic relu results, adding the expanded data

--- src/conv.py
+++ src/conv.py
@@ -90,32 +90,38 @@ def dbl_conv_tanh():
         net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
 
 def dbl_conv_relu():
-    for j in range(3):
-        print "Conv + Conv + FC, using ReLU and regularization"
-        net = Network([
-            ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28), 
-                          filter_shape=(20, 1, 5, 5), 
-                          poolsize=(2, 2),
-                          activation_fn=tanh),
-            ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12), 
-                          filter_shape=(40, 20, 5, 5), 
-                          poolsize=(2, 2),
-                          activation_fn=tanh),
-            FullyConnectedLayer(n_in=40*4*4, n_out=100, activation_fn=tanh),
-            SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
-        net.SGD(training_data, 60, mini_batch_size, 0.03, validation_data, test_data, lmbda=1.0)
-
-def dbl_conv_relu():
     for lmbda in [0.00001, 0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0]:
         for j in range(3):
             print "Conv + Conv + FC num %s, relu, with regularization %s" % (j, lmbda)
             net = Network([
                 ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28), 
                               filter_shape=(20, 1, 5, 5), 
-                              poolsize=(2, 2), activation_fn=ReLU),
+                              poolsize=(2, 2), 
+                              activation_fn=ReLU),
                 ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12), 
                               filter_shape=(40, 20, 5, 5), 
-                              poolsize=(2, 2), activation_fn=ReLU),
+                              poolsize=(2, 2), 
+                              activation_fn=ReLU),
                 FullyConnectedLayer(n_in=40*4*4, n_out=100, activation_fn=ReLU),
                 SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
             net.SGD(training_data, 60, mini_batch_size, 0.03, validation_data, test_data, lmbda=lmbda)
+
+def expanded_data():
+    expanded_training_data, _, _ = network3.load_data_shared(
+        "../data/mnist_expanded.pkl.gz")
+    for j in range(3):
+        print "Training with expanded data, run num %s" % j
+        net = Network([
+            ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28), 
+                          filter_shape=(20, 1, 5, 5), 
+                          poolsize=(2, 2), 
+                          activation_fn=ReLU),
+            ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12), 
+                          filter_shape=(40, 20, 5, 5), 
+                          poolsize=(2, 2), 
+                          activation_fn=ReLU),
+            FullyConnectedLayer(n_in=40*4*4, n_out=100, activation_fn=ReLU),
+            SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+        net.SGD(expanded_training_data, 20, mini_batch_size, 0.03, 
+                validation_data, test_data, lmbda=1.0)
+    
