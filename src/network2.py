b6cb6259908721064776ffd5783c56cb04cca7ff
gondimribeiro <gondimribeiro@gmail.com>
2015-10-30 22:22:11 -0200

    wrong weights inatialization

--- src/network2.py
+++ src/network2.py
@@ -97,7 +97,7 @@ class Network(object):
 
         """
         self.biases = [np.random.randn(y, 1) for y in self.sizes[1:]]
-        self.weights = [np.random.randn(y, x)/np.sqrt(x)
+        self.weights = [np.random.randn(y, 1)/np.sqrt(x)
                         for x, y in zip(self.sizes[:-1], self.sizes[1:])]
 
     def large_weight_initializer(self):
