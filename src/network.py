38b9ec85f689c7a3f2b58249e8f70ed1f5f2b73d
navyag2 <70657112+navyag2@users.noreply.github.com>
2021-02-09 09:58:16 +0530

    Update network.py
    Found a syntax error in the print statements of def SGD

--- src/network.py
+++ src/network.py
@@ -61,10 +61,10 @@ class Network(object):
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
             if test_data:
-                print "Epoch {0}: {1} / {2}".format(
-                    j, self.evaluate(test_data), n_test)
+                print ("Epoch {0}: {1} / {2}".format(
+                    j, self.evaluate(test_data), n_test))
             else:
-                print "Epoch {0} complete".format(j)
+                print ("Epoch {0} complete".format(j))
 
     def update_mini_batch(self, mini_batch, eta):
         """Update the network's weights and biases by applying
