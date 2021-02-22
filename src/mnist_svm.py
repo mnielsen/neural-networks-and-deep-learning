d97d69d87c2a54ed44f9568213838048a076e946
aliPMPAINT <ali.PMPAINT@tutanota.com>
2021-02-22 13:29:20 +0330

    refactored SVM model

--- src/mnist_svm.py
+++ src/mnist_svm.py
@@ -20,8 +20,8 @@ def svm_baseline():
     # test
     predictions = [int(a) for a in clf.predict(test_data[0])]
     num_correct = sum(int(a == y) for a, y in zip(predictions, test_data[1]))
-    print "Baseline classifier using an SVM."
-    print "%s of %s values correct." % (num_correct, len(test_data[1]))
+    print("Baseline classifier using an SVM.")
+    print("{0} of {1} values correct.".format(num_correct, len(test_data[1])))
 
 if __name__ == "__main__":
     svm_baseline()
