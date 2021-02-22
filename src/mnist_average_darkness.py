396577620d8700aff4a6e423e090d8e180a38c53
aliPMPAINT <ali.PMPAINT@tutanota.com>
2021-02-22 13:34:32 +0330

    refactored average_darkness model

--- src/mnist_average_darkness.py
+++ src/mnist_average_darkness.py
@@ -33,8 +33,8 @@ def main():
     # correctly
     num_correct = sum(int(guess_digit(image, avgs) == digit)
                       for image, digit in zip(test_data[0], test_data[1]))
-    print "Baseline classifier using average darkness of image."
-    print "%s of %s values correct." % (num_correct, len(test_data[1]))
+    print("Baseline classifier using average darkness of image.")
+    print("{0} of {1} values correct.".format(num_correct, len(test_data[1])))
 
 def avg_darknesses(training_data):
     """ Return a defaultdict whose keys are the digits 0 through 9.
@@ -47,7 +47,7 @@ def avg_darknesses(training_data):
         digit_counts[digit] += 1
         darknesses[digit] += sum(image)
     avgs = defaultdict(float)
-    for digit, n in digit_counts.iteritems():
+    for digit, n in digit_counts.items():
         avgs[digit] = darknesses[digit] / n
     return avgs
 
@@ -57,7 +57,7 @@ def guess_digit(image, avgs):
     assumed to be a defaultdict whose keys are 0...9, and whose values
     are the corresponding average darknesses across the training data."""
     darkness = sum(image)
-    distances = {k: abs(v-darkness) for k, v in avgs.iteritems()}
+    distances = {k: abs(v-darkness) for k, v in avgs.items()}
     return min(distances, key=distances.get)
 
 if __name__ == "__main__":
